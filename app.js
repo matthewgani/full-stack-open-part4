decb949510da6c5b0d7aafc0b8a5d68763ee4cdb
matthewgani <matthewgani2@gmail.com>
2022-06-13 16:57:50 +0800

    Refactor to add controllers, models, middleware

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-const http = require('http')
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-require('dotenv').config()
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-console.log(process.env.PORT)
-console.log(process.env.MONGODB_URI)
-mongoose.connect(mongoUrl)
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
   .then(result => {
     console.log('connected to MongoDB')
   })
@@ -27,24 +17,15 @@ mongoose.connect(mongoUrl)
   })
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.use('/api/blogs', blogsRouter)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
 
 module.exports = app
